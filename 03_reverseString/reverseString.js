21f4ca33e436fe4c9de5b655b1fc268d39e5cfce
Lippiece <seriouss7222@gmail.com>
2022-03-28 15:52:06 +0200

    Push chars into working array one by one

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,8 @@ const reverseString = function (str) {
 	let arr = str.split("");
 	let newArr = [];
 	for (let i = arr.length - 1; i >= 0; i--) {
+		const char = arr[i];
+		newArr.push(char);
 	}
 
 // Do not edit below this line
