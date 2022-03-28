d981d17930bdc03d3aff4285efc3ab6126611961
Lippiece <seriouss7222@gmail.com>
2022-03-28 15:51:33 +0200

    Create working array and iterate backwards through existing one

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
 const reverseString = function (str) {
 	let arr = str.split("");
+	let newArr = [];
+	for (let i = arr.length - 1; i >= 0; i--) {
+	}
 
 // Do not edit below this line
 module.exports = reverseString;
