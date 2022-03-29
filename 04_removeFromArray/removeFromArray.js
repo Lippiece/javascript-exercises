abf0464eca05c452a26abd134aea1d6851122ffc
Lippiece <seriouss7222@gmail.com>
2022-03-29 10:14:21 +0200

    Add cycling through array elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function (array, ...args) {
 	for (const j of args) {
+		for (let i = 0; i < array.length; i++) {	
+			if (array[i] === j) {
+				array.splice(i,1);			
+			}
 		}
 	}
 	return array;
