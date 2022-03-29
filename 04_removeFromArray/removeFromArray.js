25ad776eb935a95c5c46d467cd50a35cf7bff244
Lippiece <seriouss7222@gmail.com>
2022-03-29 08:30:36 +0200

    When cycled member is equal to target, remove it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
 const removeFromArray = function (array, target) {
 	let workingArr;
 	for (let i of array) {
+		if (i === target) {
+			workingArr = array.splice(target-1, 1);
+		}
+	}
 };
 
 // Do not edit below this line
