ff2852e646e357d772e898bc5b6dafc5adf59fdd
Lippiece <seriouss7222@gmail.com>
2022-03-30 12:14:00 +0200

    Return the sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -20,6 +20,9 @@ const sumAll = function (num1, num2) {
 		for (let i = start; i <= end; i++) {
 			output += i;
 		}
+		return output;
+	}
+};
 
 // Do not edit below this line
 module.exports = sumAll;
