2bab71f7cfac6d78a4ecfa505f3f3a2e47b3b9bd
Lippiece <seriouss7222@gmail.com>
2022-03-30 12:49:36 +0200

    Remove .prettierrc

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -10,7 +10,9 @@ const sumAll = function (num1, num2) {
 		let output = 0;
 		let start = 0;
 		let end = 0;
-		if (num1 < num2) {start = num1; end = num2;
+		if (num1 < num2) {
+			start = num1;
+			end = num2;
 		} else {
 			start = num2;
 			end = num1;
