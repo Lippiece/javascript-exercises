fe03752fe7110c8b4d169935f15b69285ac915f5
Lippiece <seriouss7222@gmail.com>
2022-03-30 12:11:50 +0200

    Check if input is number and >0 and add vars

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
 const sumAll = function (num1, num2) {
+	if (
+		typeof num1 != "number" ||
+		typeof num2 != "number" ||
+		num1 < 0 ||
+		num2 < 0
+	) {
+		return "ERROR";
+	} else {
+		let output = 0;
+		let start = 0;
+		let end = 0;
 
 // Do not edit below this line
 module.exports = sumAll;
