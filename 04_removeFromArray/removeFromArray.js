01d86186a9dd3389e27ad549a64d2d879d2f276e
Lippiece <seriouss7222@gmail.com>
2022-03-30 12:41:51 +0200

    Prettier

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,12 @@
 const removeFromArray = function (array, ...args) {
-	for (const j of args) {
-		for (let i = 0; i < array.length; i++) {	
-			if (array[i] === j) {
-				array.splice(i,1);			
-			}
-		}
-	}
-	return array;
+  for (const j of args) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === j) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
