ff7beda4d3e53080cb8ab8946261818df97ca196
Lippiece <seriouss7222@gmail.com>
2022-03-31 13:47:39 +0200

    Calc the output

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,10 @@
 const ftoc = function (input) {
+	let output = ((input - 32) * 5) / 9;
 };
 
 };
 const ctof = function (input) {
+	let output = (input * 9) / 5 + 32;
 };
 
 // Do not edit below this line
