39a3b841f1f4d9dc8f9c83b98d047e36291451f9
Lippiece <seriouss7222@gmail.com>
2022-04-09 13:24:55 +0200

    Add the simple functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,12 +1,18 @@
-const add = function () {};
+const add = function (input1, input2) {
+	return input1 + input2;
+};
 
-const subtract = function () {};
+const subtract = function (input1, input2) {
+	return input1 - input2;
+};
 
 const sum = function () {};
 
 const multiply = function () {};
 
-const power = function () {};
+const power = function (input1, input2) {
+	return Math.pow(input1, input2);
+};
 
 const factorial = function () {};
 
