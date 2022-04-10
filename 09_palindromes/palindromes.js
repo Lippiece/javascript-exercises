8bc5332494dfce0810fd1ea7193cd029cfd8536b
Lippiece <seriouss7222@gmail.com>
2022-04-10 13:30:54 +0200

    Insert the solution to analyze

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {};
+// /[^a-zA-Z]/g -- remove non-letters
+
+const palindromes = function (string) {
+	const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+	return processedString.split("").reverse().join("") == processedString;
+};
+palindromes("A big brown fox?");
 
 // Do not edit below this line
 module.exports = palindromes;
