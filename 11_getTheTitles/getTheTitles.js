4e721d522d8e1928b6df90a474e536dc6d239521
Lippiece <seriouss7222@gmail.com>
2022-04-12 12:16:10 +0200

    Push the input.title into the array and return it

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
 const getTheTitles = function (input) {
 	let newArr = [];
+	for (const key in input) {
+		const element = input[key];
+		newArr.push(element.title);
+	}
+	return newArr;
 };
 
 // Do not edit below this line
