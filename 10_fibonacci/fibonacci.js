d4ef53bfa8e0fdb18895cc49faa3fd1359684c3c
Lippiece <seriouss7222@gmail.com>
2022-04-12 12:14:19 +0200

    Sum the numbers depending on the input

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,6 +3,9 @@ const fibonacci = function (input) {
 		return "OOPS";
 	}
 	let seq = [1, 1];
+	for (let i = 2; i < parseInt(input); i++) {
+		seq.push(seq[seq.length - 1] + seq[seq.length - 2]);
+	}
 };
 
 // Do not edit below this line
