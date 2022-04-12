7cce9d1f5d253d429bfe7ecd75250a108622ee84
Lippiece <seriouss7222@gmail.com>
2022-04-12 12:17:46 +0200

    Set the end year to current if there is none

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,6 +2,15 @@ const findTheOldest = function (input) {
 	var currentTime = new Date();
 	var year = currentTime.getFullYear();
 
+	const oldest = input.reduce(
+		(a, b) => {
+			if (a.yearOfDeath === undefined) {
+				a.yearOfDeath = year;
+			}
+			if (b.yearOfDeath === undefined) {
+				b.yearOfDeath = year;
+			}
+	);
 };
 
 // Do not edit below this line
